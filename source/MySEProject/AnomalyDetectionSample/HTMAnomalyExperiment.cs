b8de7a681ccc4f2ee628354bfcf906d51ae807d0
HasanAhmed-bit <Hasanbc65@gmail.com>
2025-02-07 23:17:15 +0100

    AnomalyExperiment

--- source/MySEProject/AnomalyDetectionSample/HTMAnomalyExperiment.cs
+++ source/MySEProject/AnomalyDetectionSample/HTMAnomalyExperiment.cs
@@ -13,11 +13,6 @@ namespace AnomalyDetectionSample
 
     public class HTMAnomalyExperiment
     {
-        private readonly string _trainingCSVFolderPath;
-        private readonly string _predictingCSVFolderPath;
-        private static double _totalAccuracy = 0.0;
-        private static int _iterationCount = 0;
-        private readonly double _tolerance = 0.1;
 
 
         /// <summary>
